332e7d87d0ac1a09bd9801f238b44aeb7ca72a5a
katsuo14 <67542683+katsuo14@users.noreply.github.com>
2020-08-22 20:59:06 +0900

    auto-update_finish

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="main-chat__message-list__message-box">
+        `<div class="main-chat__message-list__message-box" data-message-id=${message.id}>
           <div class="main-chat__message-list__message-box__info">
             <div class="main-chat__message-list__message-box__info__user">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="main-chat__message-list__message-box">
+      `<div class="main-chat__message-list__message-box" data-message-id=${message.id}>
         <div class="main-chat__message-list__message-box__info">
           <div class="main-chat__message-list__message-box__info__user">
             ${message.user_name}
@@ -60,6 +60,8 @@ $(function(){
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+      $('.main-chat__message-form__form__submit').prop("disabled", false);
     });
   })
+  
 });
